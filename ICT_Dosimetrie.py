22f14621f7556e1e0c9a7a98cf41dc222e7eda6f
Julien <j-laffarguette@o-lambret.fr>
2022-10-12 17:01:37 +0200

    Update ICT_Dosimetrie.py
    Version stable simplifiée. Suppression des points cou et pubis

--- ICT_Dosimetrie.py
+++ ICT_Dosimetrie.py
@@ -13,6 +13,7 @@ except:
 from connect import *
 import numpy as np
 from easygui import multenterbox
+from datetime import datetime
 
 
 def check_roi(case, roi_to_check):
@@ -62,10 +63,7 @@ class Patient:
 
         # Récupération de la position de tous les POI sur le scanner HFS
         self.jonction = self.get_point_coords('jonction')
-        self.pubis = self.get_point_coords('pubis')
         self.genoux = self.get_point_coords('genoux')
-        self.cou = self.get_point_coords('cou')
-        self.thorax = self.get_point_coords('thorax')
         self.abdomen = self.get_point_coords('abdomen')
 
         # Création du point zero (crane) situé à 0,0,hauteur table (valeur exprimée en mm convertie en cm)
@@ -315,7 +313,7 @@ if __name__ == '__main__':
                                                                                       'ConvergenceTolerance': 1E-05})
 
             # mapping des POI d'un scanner vers l'autre
-            pois = ["jonction", "genoux", "pubis", "abdomen"]
+            pois = ["jonction", "genoux", "abdomen"]
 
             obj_patient.case.MapPoiGeometriesDeformably(PoiGeometryNames=pois,
                                                         CreateNewPois=False,
@@ -339,8 +337,7 @@ if __name__ == '__main__':
                                                             obj_patient.examinations["FFS"]])
 
         # Création des ROI
-        ROI_LIST = ['PTV_E', 'PTV_D6', 'PTV_D5', 'PTV_D4', 'PTV_D3', 'PTV_D2', 'PTV_D1', 'PTV_C', 'PTV_B', 'PTV_A',
-                    'PTV haut']
+        ROI_LIST = ['PTV FFS', 'PTV_4', 'PTV_3', 'PTV_2', 'PTV_1', "PTV HFS"]
         # Vérification de l'existance des differentes roi dans le case
         resultat = [check_roi(obj_patient.case, roi) for roi in ROI_LIST]
         # Création des ROI dans le roi set si ROI non existantes
@@ -373,10 +370,6 @@ if __name__ == '__main__':
             # créer somme des reins
             obj_patient.algebra(out_roi="Reins", in_roiA=["Rein D", "Rein G"], color="Yellow")
 
-            # Création PTVpoumons = poumons - 1cm # seulement pour le scanner HFS!
-            if direction == 'HFS':
-                obj_patient.algebra(out_roi='PTVpoumons', in_roiA=["Poumons"], margeA=-1, Type="Ptv", color="Yellow")
-
             # ------------------------------------------------------------------------------------
             # Création des cylindres
 
@@ -385,51 +378,33 @@ if __name__ == '__main__':
             x0, z0 = 0, obj_patient.zero_scan
 
             # Réalisation des PTV 2 à 5, entourant la bille jonction
-            for index, roi in enumerate(['PTV_D2', 'PTV_D3', 'PTV_D4', 'PTV_D5']):
-                # y0 est au niveau de la premiere coupe de D3
-                # la premiere coupe de D2 est donc située à -2; celle de D4 à +2 et celle de D5 à +4
+            for index, roi in enumerate(['PTV_1', 'PTV_2', 'PTV_3', 'PTV_4']):
+                # y0 est au niveau de la premiere coupe de PTV_2
+                # la premiere coupe de PTV_3 est donc située à -2; celle de D1 à +2 etc
                 # soit -2 ; 0 ; 2 ; 4 -> 4-2*i
                 yhaut = y0 + 4 - 2 * index
                 y_bas = yhaut - 2
                 obj_patient.create_cylinder_ptv(roi, yhaut, y_bas)
 
+            # Liste qui contiendra pour chaque volume, son point de départ et son point d'arrivée
             from_to = []
 
-            # Réalisation du PTV D6 : de la bille genoux jusqu'au bas du PTV D5
-            bas_PTV5 = y0 - 4  # 4 cm en dessous de la bille jonction
-            _, _, y_genoux = obj_patient.genoux
+            # lim sup et inf correspondent au points les plus hauts et bas du contour externe
+            lim_inf,lim_sup = [lim.z for lim in obj_patient.case.PatientModel.StructureSets[ct_name].RoiGeometries["External"].GetBoundingBox()]
 
-            # Réalisation du PTV D1 : de la bille pubis jusqu'au haut du PTV D2
-            haut_PTVD2 = y0 + 4  # 4 cm au dessus de la bille jonction
-            _, _, y_pubis = obj_patient.pubis
-
-            from_to.append(['PTV_D6', y_genoux, bas_PTV5])
-            from_to.append(['PTV_D1', haut_PTVD2, y_pubis])
+            # Réalisation du "PTV HFS", au-dessus du dernier PTV de la jonction, jusqu'au sommet du crane
+            haut_PTV1 = y0 + 4
+            from_to.append(["PTV HFS", lim_sup, haut_PTV1])
 
             if direction == 'HFS':
-                # Réalisation du PTV C : au dessus du PTV D1 jusqu'à 3 cm en dessous des poumons
-                sous_poumons = \
-                    obj_patient.case.PatientModel.StructureSets[ct_name].RoiGeometries["Poumons"].GetBoundingBox()[
-                        0].z  # relou, raystation intervertit y et z
-                sous_poumons -= 3  # 3cm en dessous du plus bas du poumon
+                # Création PTVpoumons = poumons - 1cm # seulement pour le scanner HFS!
+                obj_patient.algebra(out_roi='PTVpoumons', in_roiA=["Poumons"], margeA=-1, Type="Ptv",color="Yellow")
+            else :
+                # Supprimer la dérivation
+                obj_patient.case.PatientModel.RegionsOfInterest['PTVpoumons'].DeleteExpression()
 
-                # Réalisation du PTV B : au dessus du PTV C jusqu'à la bille cou
-                # Edit 18/08/2022 -> soustraction des poumons
-                _, _, y_cou = obj_patient.cou
-
-
-                # Réalisation du PTV A : au dessus du PTV B
-                tout_en_haut = y_cou + 50
-
-                from_to.append(['PTV_C', sous_poumons, y_pubis])
-                from_to.append(['PTV_B', y_cou, sous_poumons])
-                from_to.append(['PTV_A', tout_en_haut, y_cou])
-
-            elif direction == 'FFS':
-                # Réalisation du PTV E : en dessous du PTV E
-                tout_en_bas = y_genoux - 100
-
-                from_to.append(['PTV_E', y_genoux, tout_en_bas])
+            # Réalisation du PTV FFS : en dessous du PTV E
+            from_to.append(['PTV FFS', y0-4, lim_inf])
 
             # Une fois que toutes les bornes sont déterminées, on crée les PTVS
             for roi, f, t in from_to:
@@ -437,21 +412,15 @@ if __name__ == '__main__':
                 obj_patient.create_cylinder_ptv(roi, f, t)
                 print('-> OK!')
 
-            # Update de la dérivation du PTV poumons
-            obj_patient.case.PatientModel.RegionsOfInterest['PTVpoumons'].UpdateDerivedGeometry(Examination=ct,
-                                                                                                Algorithm="Auto")
-            # PTVB - PTVPoumons = PTVB
-            obj_patient.algebra(out_roi='PTV_B', in_roiA=["PTV_B"], in_roiB=["PTVpoumons"],
+            # PTV HFS - PTVPoumons = PTVB
+            obj_patient.algebra(out_roi='PTV HFS', in_roiA=["PTV HFS"], in_roiB=["PTVpoumons"],
                                 ResultOperation="Subtraction", derive=False)
 
-            # Dernière étape: soustraction des volumes deux à deux pour éviter le chevauchement des PTVS
-            ROI_LIST = ['PTV_E', 'PTV_D6', 'PTV_D5', 'PTV_D4', 'PTV_D3', 'PTV_D2', 'PTV_D1', 'PTV_C', 'PTV_B', 'PTV_A']
+            # Dernière étape : soustraction des volumes deux à deux pour éviter le chevauchement des PTVS
+            ROI_LIST = ['PTV FFS', 'PTV_4', 'PTV_3', 'PTV_2', 'PTV_1', "PTV HFS"]
             for i in range(len(ROI_LIST) - 1):
                 obj_patient.algebra(out_roi=ROI_LIST[i], in_roiA=[ROI_LIST[i]], in_roiB=[ROI_LIST[i + 1]],
                                     ResultOperation="Subtraction", derive=False)
-            if direction == 'HFS':
-                # Finalement, création du PTV haut, qui sera utilisé seul
-                obj_patient.algebra(in_roiA=['PTV_A', 'PTV_B', 'PTV_C', 'PTV_D1'], out_roi='PTV haut')
 
             # simplification des volumes pour éviter overlaps
             roi_list = [roi.Name for roi in obj_patient.case.PatientModel.RegionsOfInterest if roi.Type == "Support"]
@@ -467,10 +436,10 @@ if __name__ == '__main__':
             # FFS -> PTV D1 devient "opt_jonction"
 
             if direction == "HFS":
-                source = "PTV_D6"
+                source = "PTV FFS"
                 OAR_name = "opt_jonction"
             else:
-                source = "PTV_D1"
+                source = "PTV HFS"
                 OAR_name = "opt_jonction"
 
             # Création du volume
@@ -484,7 +453,8 @@ if __name__ == '__main__':
     #########################################################################
 
     # Création des deux plans
-    PLAN_NAMES = ["Plan ICT Head First", "Plan ICT Feet First"]
+    date = datetime.today().strftime('%Y%m%d')
+    PLAN_NAMES = [f"HFS_{date}", f"FFS_{date}"]
     BS_NAMES = ["bs_HFS", "bs_FFS"]
     PATIENT_POSITIONS = ["HeadFirstSupine", "FeetFirstSupine"]
     machine_name = "Radixact1"
@@ -609,10 +579,10 @@ if __name__ == '__main__':
         plan.PlanOptimizations[0].OptimizationParameters.DoseCalculation.ComputeFinalDose = True
 
         # if ind = 0 = HFS la prescription est attribuée au PTV haut, sinon au PTV_E pour le plan FFS
-        if ind == 0:
-            prescription_roi = 'PTV haut'
+        if direction == 'HFS':
+            prescription_roi = 'PTV HFS'
         else:
-            prescription_roi = 'PTV_E'
+            prescription_roi = 'PTV FFS'
 
         # Si la prescription existe déjà, ne pas la recréer
         if not beam_set.Prescription.PrescriptionDoseReferences:
@@ -641,27 +611,17 @@ if __name__ == '__main__':
 
         laser_rouge_HFS = "laser rouge HFS"
 
-        choix1 = "Toulouse"  # on positionne le laser rouge à deux endroits différents = relou
-        choix2 = "Lille"  # On positionne les lasers rouges et vert sur le point abdomen mais le rouge est décalé sur la bille genoux en GD
+        # On positionne les lasers rouges et vert sur le point abdomen mais le rouge est décalé sur la bille genoux en GD
+        coords_laser_rouges_HFS = (obj_patient.genoux[0], obj_patient.zero_scan, obj_patient.abdomen[2])
 
-        choix = choix2
-
-        if choix == choix1:  # du coup, ceci ne sera pas utilisé
-            coords_laser_rouges_HFS = (0, obj_patient.zero_scan, obj_patient.thorax[2])
-            # laser vert positionné 13 cm en dessous du laser rouge
-            shift = -13
-            coords_laser_vert = [0, coords_laser_rouges_HFS[1], obj_patient.thorax[2] + shift]
-        elif choix == choix2:
-            coords_laser_rouges_HFS = (obj_patient.genoux[0], obj_patient.zero_scan, obj_patient.abdomen[2])
-
-            if ind == 0:  # Plan HFS
-                coords_laser_vert = (0, obj_patient.zero_scan, obj_patient.abdomen[2])
-            elif ind == 1:  # Plan FFS
-                # Pour le plan FFS, le laser vert est positionné en AP au centre du volume PTV_E afin que le patient
-                # soit bien centré en hauteur et n'ait pas les genoux qui dépassent du cadre
-                AP_iso_FFS = obj_patient.case.PatientModel.StructureSets[obj_patient.examinations['FFS']].RoiGeometries[
-                    'PTV_E'].GetCenterOfRoi().y
-                coords_laser_vert = (0, AP_iso_FFS, obj_patient.abdomen[2])
+        if direction == 'HFS':  # Plan HFS
+            coords_laser_vert = (0, obj_patient.zero_scan, obj_patient.abdomen[2])
+        elif direction == 'FFS':  # Plan FFS
+            # Pour le plan FFS, le laser vert est positionné en AP au centre du volume PTV_E afin que le patient
+            # soit bien centré en hauteur et n'ait pas les genoux qui dépassent du cadre
+            AP_iso_FFS = obj_patient.case.PatientModel.StructureSets[obj_patient.examinations['FFS']].RoiGeometries[
+                'PTV FFS'].GetCenterOfRoi().y
+            coords_laser_vert = (0, AP_iso_FFS, obj_patient.abdomen[2])
 
         # création du laser rouge
         obj_patient.create_poi(laser_rouge_HFS, coords_laser_rouges_HFS, color='Red')
@@ -763,11 +723,7 @@ if __name__ == '__main__':
 
             # Ici on associe à chaque PTV un certain niveau par rapport à la dose de prescription. Travail avec numpy
             # pour pouvoir utiliser la méthode np.where, associant la dose à la roi
-            ptv_list = np.array(['PTV haut', 'PTV_D2', 'PTV_D3', 'PTV_D4', 'PTV_D5', 'PTVpoumons'])
-
-            # Ces deux lignes sont à réactiver si on veut utiliser tous les PTVS et plus seulement PTV haut
-            # ptv_list = np.array(
-            #     ['PTV_A', 'PTV_B', 'PTV_C', 'PTV_D1', 'PTV_D2', 'PTV_D3', 'PTV_D4', 'PTV_D5', 'PTV_D6', 'PTVpoumons'])
+            ptv_list = np.array(['PTV HFS', 'PTV_1', 'PTV_2', 'PTV_3', 'PTV_4', 'PTVpoumons'])
 
             # Définition de la contrainte aux poumons. Si la dose prescrite est supérieure à 8Gy, on applique 7.5 Gy en
             # contrainte aux poumons (vu avec SP, ça marche mieux que de mettre 8)
@@ -794,30 +750,25 @@ if __name__ == '__main__':
             obj_DFO = 0.16 * total_dose
             weight_DFO = 50
 
-            # Ces deux lignes sont à réactiver si on veut utiliser tous les PTVS et plus seulement PTV haut
-            # objectives_list = [total_dose, total_dose, total_dose, total_dose, 0.9 * total_dose, 0.63 * total_dose,
-            #                    0.37 * total_dose, 0.16 * total_dose, 0.05 * total_dose, dose_poumons]
-            # weight_list = [1,1,1,1,1,1,1,1,1,1]
-
             robustesse = False
 
         # ////////////////////////////////////////////////////////////////////////////////////////////////////////////
         # Plan FFS
         elif ind == 1:  # Plan FFS
             ptv_list = np.array(
-                ['PTV_E', 'PTV_D6', 'PTV_D5', 'PTV_D4', 'PTV_D3', 'PTV_D2'])
+                ['PTV FFS', 'PTV_4', 'PTV_3', 'PTV_2', 'PTV_1'])
 
-            objectives_list = [total_dose, total_dose, 0.9 * total_dose, 0.63 * total_dose, 0.37 * total_dose,
+            objectives_list = [total_dose, 0.9 * total_dose, 0.63 * total_dose, 0.37 * total_dose,
                                0.16 * total_dose]
 
-            weight_list = [1, 1, 1, 1, 1, 1]
+            weight_list = [1, 1, 1, 1, 1]
             robustesse = True
 
-        # ici, on crée des fonctions d'optimisation pour chacun des PTVS. Initialement, ces fonctions sont vides,
+        # Ici, on crée des fonctions d'optimisation pour chacun des PTVS. Initialement, ces fonctions sont vides,
         # elles sont remplies ensuite
         for roi in ptv_list:
 
-            liste = ["PTV haut", "PTVpoumons", "PTV_D6", "PTV_E"]
+            liste = ["PTV HFS", "PTVpoumons", "PTV FFS"]
             if any(roi in s for s in liste):  # On ne met l'uniform dose que sur ces volumes là
                 plan.PlanOptimizations[0].AddOptimizationFunction(FunctionType="UniformDose", RoiName=str(roi),
                                                                   IsConstraint=False,
